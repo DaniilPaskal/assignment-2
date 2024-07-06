e9415c439ad74d114a457fb7cedafbc94f4dda5c
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2024-07-05 22:09:19 -0400

    Reorganize files

--- react-project/src/data/CurrentUser.jsx
+++
@@ -1,6 +0,0 @@
-import { User } from "./Classes";
-
-// Global object representing current user
-var CurrentUser = new User();
-
-export default CurrentUser;
--- react-project/src/pages/Error.jsx
+++
@@ -1,8 +0,0 @@
-export default function Error() {
-    return (
-        <div className="page-content">
-            <h1>Page not found</h1>
-            <p>This address does not lead to a page!</p>
-        </div>
-    );
-}
